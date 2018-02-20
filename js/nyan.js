63a616fb1e668944e1ee64b207982ad6f4c2d1d4
redmanmale <redmanmale@gmail.com>
2018-02-20 22:33:29 +0300

    Remove redundant js code

--- js/nyan.js
+++ js/nyan.js
@@ -1,41 +1,3 @@
-var cats = ["america",
-"balloon",
-// "bday",
-// "breakfast",
-// "daft",
-// "dub",
-// "easter",
-// "elevator",
-// "fiesta",
-"gb",
-// "j5",
-"jamaicnyan",
-"jazz",
-// "manyan",
-// "melon",
-// "mexinyan",
-"mummy",
-"newyear",
-"nyancoin",
-"nyaninja",
-"original",
-// "patty",
-// "pikanyan",
-// "pirate",
-"pumpkin",
-"retro",
-"sad",
-"slomo",
-// "star",
-// "tacnayn",
-"technyancolor",
-"vday",
-"wtf",
-// "xmas",
-"zombie"];
-
-var audioOverrides = new Set(["breakfast", "easter", "melon", "mummy", "nyancoin", "original", "patty","pirate","pumpkin","zombie"]);
-
 var Sparks = function () {
 	return {
 		init: function (_combo) {
@@ -57,23 +19,7 @@ $(function() {
 	var sparks = new Sparks();
 	sparks.init($('.sparks-combo'));
 
-	var index = 0;
-
-	var ogg = document.getElementById("ogg");
-	ogg.volume = 0.6; //quiet down
-
-	$(window).click(function() {
-		index = ++index % cats.length; 
-		$("#nyan-cat").attr("src", "cats/" + cats[index] + ".gif");
-		if(!audioOverrides.has(cats[index])) {
-			$("#ogg").attr("src", "audio/" + cats[index] + ".ogg");
-			ogg.volume = 1;
-		}
-		else {
-			$("#ogg").attr("src", "audio/original.ogg");
-			ogg.volume = 0.5; //quiet down
-		}
-		ogg.load();
-	});
-
+    $("#ogg").attr("src", "media/nyan.ogg");
+    ogg.volume = 0.3;
+    ogg.load();
 });
